ffebd1e79469e51cca70de1c1609119d9cfafab8
Eslezer <samuelfr05@hotmail.com>
2025-02-09 22:41:25 +1000

    2 Functions added, celsius to fahrenheit and fahrenheit to celsius.

--- prac_02/Temperatures.py
+++ prac_02/Temperatures.py
@@ -1,26 +1,35 @@
 """
 CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
+Refactoring temperature conversion to 2 functions instead of 1
 """
+
+
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return 5 / 9 * (fahrenheit - 32)
+
 
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
+        print(f"Result: {celsius_to_fahrenheit(celsius):.2f} F")
     elif choice == "F":
-        fahrenheit = float(input("Farenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
+        fahrenheit = float(input("Fahrenheit: "))
+        print(f"Result: {fahrenheit_to_celsius(fahrenheit):.2f} C")
     else:
         print("Invalid option")
     print(MENU)
     choice = input(">>> ").upper()
+
 print("Thank you.")
