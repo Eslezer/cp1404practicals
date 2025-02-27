c6e128a05bd8b5b92b5297d5363406e26b49cbfe
Eslezer <samuelfr05@hotmail.com>
2025-02-28 04:23:14 +1000

    create quick_picks.py and add function to generate lottery quick picks

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,27 @@
+import random
+
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
+
+def main():
+    """generate and display quick picks for the user."""
+    num_picks = int(input("How many quick picks? "))
+
+    for _ in range(num_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{num:2}" for num in quick_pick))
+
+
+def generate_quick_pick():
+    """generate a single quick pick with random numbers"""
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        num = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if num not in numbers:
+            numbers.append(num)
+    numbers.sort()
+    return numbers
+
+main()
