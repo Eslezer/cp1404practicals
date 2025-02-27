7acc6bb437271fcab0b77991a779ed6e91498cb3
Eslezer <samuelfr05@hotmail.com>
2025-02-27 11:37:02 +1000

    Add "ten" as first element of list add 1 as last element print all elements except first two check if 9 is in the list

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -15,3 +15,15 @@ Testing lists
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# change the first element to "ten"
+numbers[0] = "ten"
+
+# change the last element to 1
+numbers[-1] = 1
+
+# print all elements except the first two
+print(numbers[2:])
+
+# check if 9 is an element of numbers and print the result
+print(9 in numbers)
