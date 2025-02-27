d37956c7df5ea9960dd0861a41025208ec4b348c
Eslezer <samuelfr05@hotmail.com>
2025-02-27 11:07:44 +1000

    add list_warmup.py and numbers list, along with tests

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - Practical
+Testing lists
+"""
+
+# What values do the following expressions have?
+# numbers[0] 3
+# numbers[-1] 2
+# numbers[3] 1
+# numbers[:-1] 3, 1, 4, 1, 5, 9
+# numbers[3:4] 1
+# 5 in numbers = true
+# 7 in numbers = false
+# "3" in numbers = false
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
