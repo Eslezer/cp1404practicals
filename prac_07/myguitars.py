69509fc225e4e22a0b7362f4bad3ecd5626591ef
Eslezer <samuelfr05@hotmail.com>
2025-03-28 06:27:18 +1000

    Complete first part of More Guitars exercise: Read, sort, and add guitars

---
+++ prac_07/myguitars.py
@@ -0,0 +1,49 @@
+import csv
+from guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+def load_guitars(filename):
+    guitars = []
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            name, year, cost = row[0], int(row[1]), float(row[2])
+            guitars.append(Guitar(name, year, cost))
+    return guitars
+
+def display_guitars(guitars):
+    for guitar in guitars:
+        print(guitar)
+
+def save_guitars(filename, guitars):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
+
+def add_user_guitars(guitars):
+    print("\nAdd your own guitars (leave name empty to finish):")
+    while True:
+        name = input("Name: ")
+        if not name:
+            break
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+
+def main():
+    guitars = load_guitars(FILENAME)
+    print("Original list of guitars:")
+    display_guitars(guitars)
+
+    guitars.sort()
+    print("\nSorted guitars (oldest to newest):")
+    display_guitars(guitars)
+
+    add_user_guitars(guitars)
+    save_guitars(FILENAME, guitars)
+    print("\nUpdated guitars have been saved.")
+
+if __name__ == "__main__":
+    main()
